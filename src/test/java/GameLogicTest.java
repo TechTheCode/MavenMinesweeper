4371ccd00df5a73d267b8a8efb8cea41d43dc887
TechTheCode <94386171+TechTheCode@users.noreply.github.com>
2024-01-30 15:16:18 +0000

    More tests added, testGameStart and testWinConditions

--- src/test/java/GameLogicTest.java
+++ src/test/java/GameLogicTest.java
@@ -5,10 +5,24 @@ import org.junit.jupiter.api.*;
 public class GameLogicTest {
     @Test
     public void testValidMove() {
-        GameLogic gameLogic = new GameLogic(new GameBoard(10, 10, 10));
+        GameLogic gameLogic = new GameLogic(new GameBoard(8, 8, 10));
         gameLogic.makeMove(5, 5); // Assuming this is a valid move
 
         Assertions.assertFalse(gameLogic.isGameOver(), "Game should not be over after a valid move");
         Assertions.assertFalse(gameLogic.hasWon(), "Game should not be won after just one valid move");
     }
+    @Test
+    public void testGameStart() {
+        GameLogic gameLogic = new GameLogic(new GameBoard(8, 8, 10));
+        Assertions.assertFalse(gameLogic.isGameOver(), "Game should not be over at start");
+        Assertions.assertFalse(gameLogic.hasWon(), "Game should not be won at start");
+    }
+    @Test
+    public void testWinCondition() {
+        GameLogic gameLogic = new GameLogic(new GameBoard(1, 1, 0));
+        gameLogic.makeMove(0, 0);
+        Assertions.assertTrue(gameLogic.hasWon(), "Game should be won when all non-mine tiles are revealed");
+    }
+
+
 }
